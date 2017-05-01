f30f12b1fc4abb10b9c31190ad82b1bccdd48d7a
pinkusupaidaa <starck88@gmail.com>
2017-05-01 10:42:26 +0200

    Lagt till centerpartiet och liberalerna

--- TestBootstrap/public/javascript/script.js
+++ TestBootstrap/public/javascript/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 		location.hash = '#home';
 	window.addEventListener('hashchange', function(){
+        if(window.location.hash === '#centerpartiet'){
+            $('#content').load('centerpartiet.html');
+        }
 		if(window.location.hash === '#socialdemokraterna'){
 			$('#content').load('socialdemokraterna.html');
 		}
