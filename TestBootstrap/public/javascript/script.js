6d335d7a2eae00744da240db8570b15866a5c74c
pinkusupaidaa <starck88@gmail.com>
2017-05-01 22:25:56 +0200

    test för att se om man kan ladda sidan när man copy pasteat.

--- TestBootstrap/public/javascript/script.js
+++ TestBootstrap/public/javascript/script.js
@@ -46,5 +46,52 @@ $(document).ready(function(){
             });
         }
 	});
+
+	//ett Litet test för att se om sidan laddar när man länkar till den
+    if(window.location.hash === '#centerpartiet'){
+        $('#content').load('centerpartiet.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#socialdemokraterna'){
+        $('#content').load('socialdemokraterna.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#liberalerna'){
+        $('#content').load('liberalerna.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#moderaterna'){
+        $('#content').load('moderaterna.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#vänsterpartiet'){
+        $('#content').load('vänsterpartiet.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#miljöpartiet'){
+        $('#content').load('miljöpartiet.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#kristdemokraterna'){
+        $('#content').load('kristdemokraterna.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#sverigedemokraterna'){
+        $('#content').load('sverigedemokraterna.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
+    if(window.location.hash === '#home'){
+        $('#content').load('home.html', function () {
+            $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
+        });
+    }
 });
 
