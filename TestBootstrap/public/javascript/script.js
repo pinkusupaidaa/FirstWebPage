7e3a66be6da5c1eb21f24f55c3f4f1f724fee921
pinkusupaidaa <starck88@gmail.com>
2017-05-04 22:26:41 +0200

    snyggare utseende till mobil

--- TestBootstrap/public/javascript/script.js
+++ TestBootstrap/public/javascript/script.js
@@ -103,5 +103,33 @@ $(document).ready(function(){
             $('html, body').animate({ scrollTop: $('#testFocus').offset().top }, 'slow');
         });
     }
+
+    // FUNKTION FÖR ATT "GO TO TOP" BTN SKA FLJA MED NÄR MAN SCROLLAR
+    (function($) {
+        var element = $('.goToTopbtn'),
+            originalY = element.offset().top;
+
+        // Space between element and top of screen (when scrolling)
+        var topMargin = -350;
+
+        // Should probably be set in CSS; but here just for emphasis
+        element.css('position', 'relative');
+
+        $(window).on('scroll', function(event) {
+            var scrollTop = $(window).scrollTop();
+
+            element.stop(false, false).animate({
+                top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+            }, 300);
+        });
+    })(jQuery);
+
+    $('#EnKnapp').on('click', function () {
+        $('html, body').animate ({
+            scrollTop: $('#zeTOP').offset().top
+        }, 800);
+    });
 });
 
